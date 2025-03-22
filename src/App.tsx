39ef445ecfabdd7d191e3512a6f44651ad4cd457
Erick-Albert1 <erick03plus@gmail.com>
2025-03-22 16:07:52 -0300

    fazendo a Estrutura e estilização utilizando framework(TAILWIND CSS) para rotas usando react-Router-dom, e salvando dados de cadastro no firebase.

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from "react-router-dom"
+
+import { Admin } from "./pages/admin"
+import { Home } from "./pages/home"
+import { Login } from "./pages/login"
+import { Networks } from "./pages/networks"
+
+import { Private } from "./routes/private"
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<Home/>
+  },
+  {
+    path:"/login",
+    element: <Login/>
+  },
+  {
+    path:"/admin",
+    element:<Private><Admin/></Private>
+  },
+  {
+    path:"/admin/social",
+    element: <Private><Networks/></Private>
+  }
+])
+
+export{router};
---
+++ src/services/firebaseConnections.ts
@@ -0,0 +1,21 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCKQCdYvRDTd9AQIw8IUOpxFHqB7xLPmXQ",
+  authDomain: "react-links-c784a.firebaseapp.com",
+  projectId: "react-links-c784a",
+  storageBucket: "react-links-c784a.firebasestorage.app",
+  messagingSenderId: "499323953148",
+  appId: "1:499323953148:web:815b82898bd57a4c0a5c79"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+const db = getFirestore(app);
+
+export{auth, db};
